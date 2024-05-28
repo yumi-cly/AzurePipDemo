847df0adf4785494cb5c98ae14b300fc644fabc0
eldong <eldon@sixarrows.net>
2024-05-28 15:18:21 -0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 8080
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
+//app.get('/', (req, res) => {
+//  res.send('Hello World!')
+//})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
