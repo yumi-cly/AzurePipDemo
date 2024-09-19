2342fad6182bcbc9b0e72beff4ef9935b0cafc9e
Vectorjay <victor.onyia.o@gmail.com>
2024-09-19 08:20:55 +0100

    First update

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('What a wonderful world!! keep pushing keep learning!')
 })
 
 app.listen(port, () => {
